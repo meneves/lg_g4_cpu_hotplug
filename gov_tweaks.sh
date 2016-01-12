5cd2152cab5b2a8d838e8fcb49a38209d467e947
meneves <meneves@gmail.com>
2016-01-12 22:57:30 +0100

    Update gov_tweaks.sh
    +autodetect governor
    +cafactive governor support
    +some fixes

--- gov_tweaks.sh
+++ gov_tweaks.sh
@@ -1,21 +1,34 @@
 #!/system/bin/sh
 
-echo 0 > /sys/devices/system/cpu/cpu1/online
-echo 0 > /sys/devices/system/cpu/cpu2/online
-echo 0 > /sys/devices/system/cpu/cpu3/online
+# list of supported governors (interactive based for now)
+for GOV in interactive cafactive
+do
+        if [ -d /sys/devices/system/cpu/cpu0/cpufreq/$GOV ];
+        then
+                governor=$GOV
+                echo governor=$GOV
+        fi
+done
 
-usleep 100000
+timer_rate=50000
+above_hispeed_delay="50000 960000:150000"
+hispeed_freq="633000"
+target_loads=110
+min_sample_time=500000
+timer_slack=500000
+go_hispeed_load=100
+io_is_busy=0
+max_freq_hysteresis=0
 
-echo "50000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
-echo "50000 960000:150000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-echo "600000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
-echo "110" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
-echo "500000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
-echo "500000" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_slack
-echo "999999" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
-echo "0" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-echo "0" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
-
-echo 1 > /sys/devices/system/cpu/cpu1/online
-echo 1 > /sys/devices/system/cpu/cpu2/online
-echo 1 > /sys/devices/system/cpu/cpu3/online
+for PR in 0 1 2 3
+do
+        echo $timer_rate > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/timer_rate
+        echo $above_hispeed_delay > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/above_hispeed_delay
+        echo $hispeed_freq > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/hispeed_freq
+        echo $target_loads > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/target_loads
+        echo $min_sample_time > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/min_sample_time
+        echo $timer_slack > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/timer_slack
+        echo $go_hispeed_load > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/go_hispeed_load
+        echo $io_is_busy > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/io_is_busy
+        echo $max_freq_hysteresis > /sys/devices/system/cpu/cpu${PR}/cpufreq/$governor/max_freq_hysteresis
+done
